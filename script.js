1ddb88b7376a81a22eb9d768476deddbfd47a3b5
Chris Gibson <chrischarlesgibson@gmail.com>
2022-09-28 14:08:29 -0500

    add gitignore and sccript

---
+++ script.js
@@ -0,0 +1,27 @@
+//psuedocode
+/* 
+need variable R, P and S 
+
+
+
+prompt-select R,S or P
+
+if user selects R,S or P. The computor will generate random response. if they dont pick either rsp, then alert pops up telling user to pick one.
+
+within S we need condition that says its > P , but < R
+
+within P we need condition that says its > R , but < S
+
+within R we need condition that says its > S , but < p
+
+
+math.random for computer variable which will be S, P or R
+
+
+
+
+*/
+
+var r;
+var p;
+var s;
